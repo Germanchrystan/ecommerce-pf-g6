eb6fe0a41001456f048c47a1dbefc546f4b62fc4
PiteIbarbia <juanignacioibarbia@gmail.com>
2021-05-05 12:47:51 -0300

    Product collection done!

--- api/index.js
+++ api/index.js
@@ -4,5 +4,5 @@ const server = require("./src/app.js");
 // Syncing all the models at once.
 
 server.listen(process.env.PORT || 3001, () => {
-  console.log("%s listening at " + process.env.PORT || 3001); // eslint-disable-line no-console
+  console.log("Listening at port " + process.env.PORT || 3001); // eslint-disable-line no-console
 });
--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -1,10 +1,38 @@
 // modelo db con mongo
+const { ObjectID } = require('bson');
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-  name: { type: 'String', required: true },
+  name: {
+    type: String, 
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  stock: {
+    type: Number,
+    required: true
+  },
+  img: {
+    type: Object,
+    required: true
+  },
+  size: {
+    type: Array,
+    requred: true
+  },
+  color: {
+    typ: Array,
+    required: true
+  }
   
 });
 
